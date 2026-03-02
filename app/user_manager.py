eeb3b327f9abbbdb491aa323204c947fcc7aba5a
alae <alaeessarhir@gmail.com>
2026-03-02 16:40:16 +0100

    user8 manager

--- app/user_manager.py
+++ app/user_manager.py
@@ -1,18 +1,23 @@
 class UserManager:
-    def __init__(self):
+
+    def _init_(self):
         self.users = []
+
     def add_user(self, username):
         if not username:
-            raise ValueError("Username cannot be empty")
+            raise ValueError("le nom d'utilisateur est  obligatoire")
         if username in self.users:
-            raise ValueError("User already exists")
-        self.users.append(username) 
+            raise ValueError("le nom d'utilisateur existe déjà")
+        self.users.append(username)
+
     def remove_user(self, username):
         if username not in self.users:
-            return ValueError("User does not exist")
-        self.users.remove(username) 
+            raise ValueError("le nom d'utilisateur n'existe pas")
+        self.users.remove(username)
+
     def count_users(self):
-        return len(self.users) 
-      
-def count_total_users(self):
-    return len(self.users)
+        return len(self.users)
+
+
+def count_total_users(users):
+    return len(users)
